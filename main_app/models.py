a877cf445c3538fd6cc3bfabe34fc337039e5a95
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-01 21:07:40 -0700

    made first model of Guitar

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Guitar(models.Model):
+
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=100)
+    #Refactor later to provide ist of years
+    year = models.IntegerField()
+    #Refactor later to provide decimal
+    price = models.IntegerField()
+    string_count = models.IntegerField()
+    color = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.brand
+
+    class Meta:
+        oredering = ['brand']
