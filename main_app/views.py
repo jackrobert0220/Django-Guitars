3ff9663a0391a6f0bd58ddf801f614ca01582382
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-01 20:37:47 -0700

    started guitarlist html

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
@@ -10,3 +11,24 @@ class Home(TemplateView):
 
 class About(View):
     template_name = "about.html"
+
+class GuitarList(TemplateView):
+    template_name = 'guitarlist.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["guitars"] = guitars
+        return context
+
+class Guitar:
+    def __init__(self, brand, model, year, price, string_count, color):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.price = price
+        self.string_count = string_count
+        self.color = color
+
+guitars = [
+    Guitar("Fender", "Telecaster", 1958, 1776.09, 6, "white"),
+]
