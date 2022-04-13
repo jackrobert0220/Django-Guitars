4961d14ca44024b70bf594a7aad1c2ca87c0d96a
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-12 17:12:31 -0700

    resolving image issue

--- djangoguitars/settings.py
+++ djangoguitars/settings.py
@@ -149,9 +149,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
