4ce754cc071168707013717e1df7cfea5f9be47f
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-12 17:20:58 -0700

    still fixing urls

--- djangoguitars/settings.py
+++ djangoguitars/settings.py
@@ -154,7 +154,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
